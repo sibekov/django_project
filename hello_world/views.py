61a5cda6b9cc4dc2d223a15c9d2070ba999f2af8
sibekov <>
2023-12-16 21:19:58 +0000

    add methods from request arguement

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http  import HttpResponse
+from django.http import HttpResponse
 
 # Create your views here.
+
+
 def index(request):
-    return HttpResponse("Hellow,world!")
+    if request.method == "POST":
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(request.method)
